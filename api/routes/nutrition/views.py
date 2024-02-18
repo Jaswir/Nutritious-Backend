14df49f43fab017ee19a80f058d39a5af78f8cd5
Slater Bohm <slaterbohm@slaters-mbp.lan>
2024-02-18 12:31:53 -0500

    add base64 endpoint

--- api/routes/nutrition/views.py
+++ api/routes/nutrition/views.py
@@ -8,7 +8,6 @@ from api.service.main import get_ingredients_from_image
 
 router = APIRouter()
 
-
 class ImageData(BaseModel):
     image_base64: str
 @router.post("/get_ingredients_from_image")
@@ -20,6 +19,20 @@ async def create_upload_file(file: UploadFile = File(...)):
         base64_image = await convert_file_to_base64(file)
         ingredients = await get_ingredients_from_image(base64_image)
 
-        return {"filename": ingredients}
+        return {"ingredients": ingredients}
+    except Exception as e:
+        return JSONResponse(content={"message": str(e)}, status_code=500)
+
+
+@router.post("/get_ingredients_from_image_base64")
+async def create_upload_file(image_data: ImageData):
+    """
+    Accepts a base64 encoded image directly in the request body.
+    """
+    try:
+        # Assuming get_ingredients_from_image is already an async function that accepts a base64 string
+        ingredients = await get_ingredients_from_image(image_data.base64_image)
+
+        return {"ingredients": ingredients}
     except Exception as e:
         return JSONResponse(content={"message": str(e)}, status_code=500)
